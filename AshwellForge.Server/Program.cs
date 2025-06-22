caf392b32bd300b51267cbfc574dba624e6e3251
Asyraf <asyzruffz@gmail.com>
2025-06-23 06:55:21 +0800

    Add admin panel ui

--- AshwellForge.Server/Program.cs
+++ AshwellForge.Server/Program.cs
@@ -1,6 +1,21 @@
+using LiveStreamingServerNet;
+using LiveStreamingServerNet.AdminPanelUI;
+using LiveStreamingServerNet.Flv.Installer;
+using LiveStreamingServerNet.Standalone;
+using LiveStreamingServerNet.Standalone.Installer;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLiveStreamingServer(
+    new IPEndPoint(IPAddress.Any, 1935),
+    options => options.AddStandaloneServices().AddFlv()
+);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpFlv();
+app.MapStandaloneServerApiEndPoints();
+app.UseAdminPanelUI(new AdminPanelUIOptions { BasePath = "/ui", HasHttpFlvPreview = true });
 
 app.Run();
