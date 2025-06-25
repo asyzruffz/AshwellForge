d2c4628cbf68ed535942ce8700271ea8e48b6e4a
Asyraf <asyzruffz@gmail.com>
2025-06-25 12:44:36 +0800

    Add datagrid for streams

---
+++ AshwellForge.Admin/Models/StreamEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace AshwellForge.Admin.Models;
+
+public record StreamEntry(
+    string StreamPath,
+    string Resolution,
+    string Framerate,
+    string VideoCodec,
+    string AudioCodec,
+    string SampleRate,
+    string Channels,
+    string Subscribers,
+    string StartTime);
